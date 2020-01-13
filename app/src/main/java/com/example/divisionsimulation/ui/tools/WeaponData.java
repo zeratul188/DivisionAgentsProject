5745461a7514eecdaf97abe761e2f971bc4478dd
zeratul188 <zeratul188@naver.com>
2020-01-13 11:46:47 +0900

    20200113-1146 added.

---
+++ app/src/main/java/com/example/divisionsimulation/ui/tools/WeaponData.java
@@ -0,0 +1,13 @@
+package com.example.divisionsimulation.ui.tools;
+
+class WeaponData {
+    private String[] weapontype = {"돌격 소총", "소총", "지정 사수 소총", "기관단총", "경기관총", "산탄총", "권총"}; //type : 7
+    private String[] weaponname1 = {"경찰용 M4", "AK-M", "ACR", "FAL", "CTAR-21", "F2000", "AUG A3-CQC", "P416", "G36", "FAMAS 2010", "Mk.16", "Carbine 7"}; //type : 12
+    private String[] weaponname2 = {"경량형 M4", "도심 MDR", "ACR SS", "M1A", "MK.17", "USC .45 ACP", "M16A2", "1886", "SIG 716"}; //type : 9
+    private String[] weaponname3 = {"M700", "SRS", "M44", "SR-1", "SVD", "SOCOM MK20 SSR"}; //type : 6
+    private String[] weaponname4 = {"AUG AS3 PARA XS", "T821", "UMP45", "MP5A2", "MP7", "MPX", "P90", "SMG-9", "PP-19", "벡터", "M1928 톰슨"}; //TYPE : 11
+    private String[] weaponname5 = {"M60", "M249", "L86 LSW", "RPK-74", "MG5", "Stoner LAMG"}; //TYPE : 6
+    private String[] weaponname6 = {"더블 배럴 샷건", "M870", "Super 90", "SASG-12", "ACS-12", "SPAS-12", "KSG"}; //TYPE : 7
+    private String[] weaponname7 = {"Diceros", "586 매그넘", "M1911", "PF45", "M9", "Px4 Storm", "X-45", "93R", "D50", "P320 XCompat", "Double Barrel Sawed Off"}; //type : 11
+    private String[] specialweaponname = {"독수리를 거느린 자", "무자비", "다이아몬드백", "네메시스", "촉새", "역병", "달콤한 꿈", "자유"}; //type : 8
+}
