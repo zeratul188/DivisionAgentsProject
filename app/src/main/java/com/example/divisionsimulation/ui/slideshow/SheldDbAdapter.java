7f56ea312886ad45fb6b6bb94d0071bb2f5df330
zerat <zeratul188@naver.com>
2020-06-22 21:49:21 +0900

    20200622 added.

---
+++ app/src/main/java/com/example/divisionsimulation/ui/slideshow/SheldDbAdapter.java
@@ -0,0 +1,134 @@
+package com.example.divisionsimulation.ui.slideshow;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+public class SheldDbAdapter {
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_NAME = "NAME";
+    public static final String KEY_FIRST = "FIRST";
+    public static final String KEY_SECOND = "SECOND";
+    public static final String KEY_THIRD = "THIRD";
+    public static final String KEY_CORE = "CORE";
+    public static final String KEY_SUB = "SUB";
+    public static final String KEY_TYPE = "TYPE";
+    public static final String KEY_VEST = "VEST";
+    public static final String KEY_BACKPACK = "BACKPACK";
+    public static final String KEY_IMAGE = "IMAGE";
+
+    private static final String DATABASE_CREATE = "create table SHELD (_id integer primary key, "
+            +"NAME text not null, FIRST text, SECOND text, THIRD text, CORE text not null, SUB text not null, "
+            +"TYPE text not null, VEST text, BACKPACK text, IMAGE text);";
+
+    private static final String DATABASE_NAME = "DIVISION_SHELD";
+    private static final String DATABASE_TABLE = "SHELD";
+    private static final int DATABASE_VERSION = 2;
+    private final Context mCtx;
+
+    private DatabaseHelper myDBHelper;
+    private SQLiteDatabase sqlDB;
+
+    public SheldDbAdapter(Context mCtx) {
+        this.mCtx = mCtx;
+    }
+
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion
+                    + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS SHELD");
+            onCreate(db);
+        }
+    }
+
+    public SheldDbAdapter open() throws SQLException {
+        myDBHelper = new DatabaseHelper(mCtx);
+        sqlDB = myDBHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void databaseReset() {
+        sqlDB.delete(DATABASE_TABLE, null, null);
+    }
+
+    public void close() {
+        myDBHelper.close();
+    }
+
+    public long createWeapon(String name, String first, String second, String third, String core, String sub, String type, String vest, String backpack, String image) {
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_FIRST, first);
+        values.put(KEY_SECOND, second);
+        values.put(KEY_THIRD, third);
+        values.put(KEY_CORE, core);
+        values.put(KEY_SUB, sub);
+        values.put(KEY_TYPE, type);
+        values.put(KEY_VEST, vest);
+        values.put(KEY_BACKPACK, backpack);
+        values.put(KEY_IMAGE, image);
+        return sqlDB.insert(DATABASE_TABLE, null, values);
+    }
+
+    public boolean deleteWeapon(long rowId) {
+        Log.i("Delete called.", "value___"+rowId);
+        return sqlDB.delete(DATABASE_TABLE, KEY_ROWID+"="+rowId, null) > 0;
+    }
+
+    public Cursor fetchAllWeapon() {
+        return sqlDB.query(DATABASE_TABLE, new String[] {KEY_ROWID, KEY_NAME, KEY_FIRST, KEY_SECOND, KEY_THIRD, KEY_CORE, KEY_SUB, KEY_TYPE, KEY_VEST, KEY_BACKPACK, KEY_IMAGE}, null, null, null, null, null);
+    }
+
+    public Cursor fetchWeapon(long rowId) throws SQLException {
+        Cursor cursor = sqlDB.query(true, DATABASE_TABLE, new String[] {KEY_ROWID, KEY_NAME, KEY_FIRST, KEY_SECOND, KEY_THIRD, KEY_CORE, KEY_SUB, KEY_TYPE, KEY_VEST, KEY_BACKPACK, KEY_IMAGE}, KEY_ROWID+"="+rowId, null, null, null, null, null);
+        if (cursor != null) cursor.moveToFirst();
+        return cursor;
+    }
+
+    public int getCount() {
+        Cursor cursor = sqlDB.rawQuery("select * from "+DATABASE_TABLE+";", null);
+        int count = 0;
+        if (cursor != null) count = cursor.getCount();
+        return count;
+    }
+
+    public int getAllCount() {
+        Cursor cursor = sqlDB.rawQuery("select * from "+DATABASE_TABLE+";", null);
+        int cnt = cursor.getCount();
+        return cnt;
+    }
+
+    public boolean updateWeapon(long rowId, String name, String first, String second, String third, String core, String sub, String type, String vest, String backpack, String image) {
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_FIRST, first);
+        values.put(KEY_SECOND, second);
+        values.put(KEY_THIRD, third);
+        values.put(KEY_CORE, core);
+        values.put(KEY_SUB, sub);
+        values.put(KEY_TYPE, type);
+        values.put(KEY_VEST, vest);
+        values.put(KEY_BACKPACK, backpack);
+        values.put(KEY_IMAGE, image);
+        return sqlDB.update(DATABASE_TABLE, values, KEY_ROWID+"="+rowId, null) > 0;
+    }
+}
--- app/src/main/java/com/example/divisionsimulation/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/divisionsimulation/ui/slideshow/SlideshowFragment.java
@@ -1,22 +1,41 @@
 package com.example.divisionsimulation.ui.slideshow;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.divisionsimulation.R;
+import com.example.divisionsimulation.ui.gallery.WeaponListActivity;
+
+import java.io.InputStream;
+
+import jxl.Sheet;
+import jxl.Workbook;
 
 public class SlideshowFragment extends Fragment {
+    
+    private SheldDbAdapter sheldAdapter;
+    private LinearLayout mainLayout;
+    
+    private int[] images = new int[28];
 
     private SlideshowViewModel slideshowViewModel;
-    //private Button[] btnSheldlist = new Button[6];
     private Button[] btnSheldoption = new Button[2];
     private int temp;
 
@@ -25,18 +44,93 @@ public class SlideshowFragment extends Fragment {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
+        
+        mainLayout = root.findViewById(R.id.mainLayout);
 
-        /*for (int i = 0; i < btnSheldlist.length; i++) {
-            temp = getResources().getIdentifier("btnSheldlist"+(i+1), "id", getActivity().getPackageName());
-            btnSheldlist[i] = root.findViewById(temp);
-        }*/
+        for (int i = 0; i < images.length; i++) images[i] = getActivity().getResources().getIdentifier("eq"+(i+1), "drawable", getActivity().getPackageName());
+        
+        sheldAdapter = new SheldDbAdapter(getActivity());
+        copyExcelDataToDatabase(images);
+
+        sheldAdapter.open();
+
+        Cursor cursor;
+        View view;
+        cursor = sheldAdapter.fetchAllWeapon();
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            String name = cursor.getString(1);
+            String first = cursor.getString(2);
+            String second = cursor.getString(3);
+            String third = cursor.getString(4);
+            String core = cursor.getString(5);
+            String sub = cursor.getString(6);
+            String type = cursor.getString(7);
+            String vest = cursor.getString(8);
+            String backpack = cursor.getString(9);
+            int image = Integer.parseInt(cursor.getString(10));
+            cursor.moveToNext();
+
+            view = getLayoutInflater().inflate(R.layout.shelditem, null);
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            param.bottomMargin = 50;
+            view.setLayoutParams(param);
+
+            final TextView txtName = view.findViewById(R.id.txtName);
+            final View viewLine = view.findViewById(R.id.viewLine);
+            final ImageView imgIcon = view.findViewById(R.id.imgIcon);
+            final TextView txtFirstInfo = view.findViewById(R.id.txtFirstInfo);
+            final TextView txtSecondInfo = view.findViewById(R.id.txtSecondInfo);
+            final TextView txtThirdInfo = view.findViewById(R.id.txtThirdInfo);
+            final TextView txtFirst = view.findViewById(R.id.txtFirst);
+            final TextView txtSecond = view.findViewById(R.id.txtSecond);
+            final TextView txtThird = view.findViewById(R.id.txtThird);
+            final TextView txtCore = view.findViewById(R.id.txtCore);
+            final TextView txtSub = view.findViewById(R.id.txtSub);
+            final LinearLayout layoutGearOptions = view.findViewById(R.id.layoutGearOptions);
+
+            if (type.equals("1")) {
+                viewLine.setBackgroundColor(Color.parseColor("#04ff8c"));
+
+                txtName.setTextColor(Color.parseColor("#04ff8c"));
+
+                final TextView txtVest = view.findViewById(R.id.txtVest);
+                final TextView txtBackpack = view.findViewById(R.id.txtBackpack);
+
+                txtFirstInfo.setText("2 세트 +:");
+                txtSecondInfo.setText("3 세트 +:");
+                txtThirdInfo.setText("4 세트 +:");
+
+                layoutGearOptions.setVisibility(View.VISIBLE);
+
+                txtVest.setText(vest);
+                txtBackpack.setText(backpack);
+            } else {
+                viewLine.setBackgroundColor(Color.parseColor("#fdae13"));
+
+                txtName.setTextColor(Color.parseColor("#fdae13"));
+
+                layoutGearOptions.setVisibility(View.GONE);
+
+                txtFirstInfo.setText("1 세트 +:");
+                txtSecondInfo.setText("2 세트 +:");
+                txtThirdInfo.setText("3 세트 +:");
+            }
+
+            imgIcon.setImageResource(image);
+            txtFirst.setText(first);
+            txtSecond.setText(second);
+            txtThird.setText(third);
+            txtCore.setText(core);
+            txtSub.setText(sub);
+            txtName.setText(name);
+
+            mainLayout.addView(view);
+        }
+
+        sheldAdapter.close();
+        
         for (int i = 0; i < btnSheldoption.length; i++) {
             temp = getResources().getIdentifier("btnSheldoption"+(i+1), "id", getActivity().getPackageName());
             btnSheldoption[i] = root.findViewById(temp);
@@ -55,28 +149,56 @@ public class SlideshowFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        /*btnSheldoption[2].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SheldOption3Activity.class);
-                startActivity(intent);
-            }
-        });
-        btnSheldoption[3].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SheldOption4Activity.class);
-                startActivity(intent);
-            }
-        });
-        btnSheldoption[4].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SheldOption5Activity.class);
-                startActivity(intent);
-            }
-        });*/
 
         return root;
+    }
+
+    private void copyExcelDataToDatabase(int[] images) {
+        Log.w("ExcelToDatabase", "copyExcelDataToDatabase()");
+
+        Workbook workbook = null;
+        Sheet sheet = null;
+
+        try {
+            InputStream is = getActivity().getBaseContext().getResources().getAssets().open("sheld.xls");
+            workbook = Workbook.getWorkbook(is);
+
+            if (workbook != null) {
+                sheet = workbook.getSheet(0);
+                if (sheet != null) {
+                    int nMaxColumn = 9;
+                    int nRowStartIndex = 0;
+                    int nRowEndIndex = sheet.getColumn(nMaxColumn-1).length - 1;
+                    int nColumnStartIndex = 0;
+                    int nColumnEndIndex = sheet.getRow(1).length - 1;
+
+                    sheldAdapter.open();
+                    sheldAdapter.databaseReset();
+
+                    for (int nRow = nRowStartIndex; nRow <= nRowEndIndex; nRow++) {
+                        String name = sheet.getCell(nColumnStartIndex, nRow).getContents();
+                        String first = sheet.getCell(nColumnStartIndex+1, nRow).getContents();
+                        String second = sheet.getCell(nColumnStartIndex+2, nRow).getContents();
+                        String third = sheet.getCell(nColumnStartIndex+3, nRow).getContents();
+                        String core = sheet.getCell(nColumnStartIndex+4, nRow).getContents();
+                        String sub = sheet.getCell(nColumnStartIndex+5, nRow).getContents();
+                        String type = sheet.getCell(nColumnStartIndex+6, nRow).getContents();
+                        String vest = sheet.getCell(nColumnStartIndex+7, nRow).getContents();
+                        String backpack = sheet.getCell(nColumnStartIndex+8, nRow).getContents();
+                        String image = Integer.toString(images[nRow]);
+
+                        sheldAdapter.createWeapon(name, first, second, third, core, sub, type, vest, backpack, image);
+                    }
+
+                    sheldAdapter.close();
+                    //Toast.makeText(getApplicationContext(), "불러오기 성공", Toast.LENGTH_SHORT).show();
+                } else System.out.println("Sheet is null!!!");
+            } else System.out.println("WorkBook is null!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getActivity(), "불러오기 오류", Toast.LENGTH_SHORT).show();
+        } finally {
+            if (workbook != null) workbook.close();
+        }
     }
 }
